1a2eac651c086a196b4425bf569e5e840f9dfd0d
justuswang <justus.wang@foxmail.com>
2014-02-23 21:31:32 +0800

    Init the project

---
+++ db/migrate/20140223123128_create_comment.rb
@@ -0,0 +1,10 @@
+class CreateComment < ActiveRecord::Migration
+  def change
+    create_table :comments do |t|
+    	t.text :body
+    	t.references :user, index: true
+    	t.references :post, index: true
+    	t.timestamps
+    end
+  end
+end
