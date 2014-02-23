2dd9c3c4f19b5f1dd60e3724be29b8e2cc3a05a8
justuswang <justus.wang@foxmail.com>
2014-02-23 22:23:53 +0800

    fix a typo

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,5 +2,5 @@ class Post < ActiveRecord::Base
   belongs_to :user
   has_many :comments
   has_many :post_categories
-  has_many :categories, through :post_categories
+  has_many :categories, through: :post_categories
 end
