3992ca0584a1e20889621517aa0e9cf3c3ca1ab9
justuswang <justus.wang@foxmail.com>
2014-03-04 23:31:31 +0800

    Move redundant code to befor_action method

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,10 +1,11 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: [:show, :edit, :update]
+
   def index
     @posts = Post.all
   end
 
   def show
-    @post = Post.find(params[:id])
   end
 
   def new
@@ -23,11 +24,9 @@ class PostsController < ApplicationController
   end
 
   def edit
-    @post = Post.find(params[:id])
   end
 
   def update
-    @post = Post.find(params[:id])
     if @post.update(post_params)
       flash[:notice] = "The post was updated"
       redirect_to posts_path
@@ -38,5 +37,9 @@ class PostsController < ApplicationController
 
   def post_params
     params.require(:post).permit(:title, :url, :creator, :description)
+  end
+
+  def set_post
+    @post = Post.find(params[:id])
   end
 end
