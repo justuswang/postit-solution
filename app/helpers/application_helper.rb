10bf68609342d95daa300305983c6ac0542c1214
justuswang <justus.wang@foxmail.com>
2014-03-08 16:35:23 +0800

    Add helpers

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  def fix_url(str)
+    str.starts_with?('http://') ? str : "http://#{str}"
+  end
+
+  def display_datetime(datetime)
+    datetime.strftime("%m/%d/%Y %l:%M%P %Z")
+  end
 end
