1a6ace433248e20d39e5b88a89740fc84154ca15
justuswang <justus.wang@foxmail.com>
2014-04-01 21:19:03 +0800

    add gem for voteable

--- lib/voteable.rb
+++ lib/voteable.rb
@@ -1,21 +1,21 @@
 # Using Concerns
-module Voteable
-  extend ActiveSupport::Concern
-  included do
-    has_many :votes, as: :voteable
-  end
+# module Voteable
+#   extend ActiveSupport::Concern
+#   included do
+#     has_many :votes, as: :voteable
+#   end
 
-  def total_votes
-    self.up_votes - self.down_votes 
-  end
+#   def total_votes
+#     self.up_votes - self.down_votes 
+#   end
 
-  def up_votes
-    self.votes.where(vote: true).size
-  end
-  def down_votes
-    self.votes.where(vote: false).size
-  end
-end
+#   def up_votes
+#     self.votes.where(vote: true).size
+#   end
+#   def down_votes
+#     self.votes.where(vote: false).size
+#   end
+# end
 
 # Using normal metaprogramming
 =begin
